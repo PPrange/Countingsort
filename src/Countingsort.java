583bd63e7b998522d9a6beebf4de4d91f53d7280
PPrange <paul-christian.prange@uni-weimar.de>
2020-05-24 10:27:28 +0200

    Countingsort changed some names

--- src/Countingsort.java
+++ src/Countingsort.java
@@ -13,34 +13,34 @@ class CountingSort
 		//finds biggest variable in array
 		int max = 0;
 		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] > max) max = arr[i] +10;
+			if(arr[i] > max) max = arr[i] + 1;
 		}
 
 		// Create a count array to store count of individual
-		// characters and initialize count array as 0 
+		// characters and initialize count array as 0
 		int count[] = new int[max];
 		for (int i=0; i<max; ++i)
 			count[i] = 0;
 
-		// store count of each character 
+		// store count of each character
 		for (int i=0; i<n; ++i)
 			++count[arr[i]];
 
-		// Change count[i] so that count[i] now contains actual 
-		// position of this character in output array 
+		// Change count[i] so that count[i] now contains actual
+		// position of this character in output array
 		for (int i=1; i<=max-1; ++i)
 			count[i] += count[i-1];
 
-		// Build the output character array 
-		// To make it stable we are operating in reverse order. 
+		// Build the output character array
+		// To make it stable we are operating in reverse order.
 		for (int i = n-1; i>=0; i--)
 		{
 			output[count[arr[i]]-1] = arr[i];
 			--count[arr[i]];
 		}
 
-		// Copy the output array to arr, so that arr now 
-		// contains sorted characters 
+		// Copy the output array to arr, so that arr now
+		// contains sorted characters
 		for (int i = 0; i<n; ++i)
 			arr[i] = output[i];
 	}
@@ -50,28 +50,28 @@ class CountingSort
 	{
 		Scanner s = new Scanner(System.in);
 
-		System.out.println("Enter the length of the array:");
+		System.out.println("Enter the amount of numbers to sort:");
 		int length = s.nextInt();
 
-		System.out.println("Enter the elements of the array:");
-		int[] arr = new int[length];
+		System.out.println("Enter the numbers to sort:");
+		int[] array = new int[length];
 		for(int i=0; i<length; i++ ) {
-			arr[i] = s.nextInt();
+			array[i] = s.nextInt();
 		}
 
-		CountingSort ob = new CountingSort();
+		CountingSort sortMe = new CountingSort();
 
-		ob.sort(arr);
+		sortMe.sort(array);
 
-		for (int i=0; i<arr.length; ++i) {
+		for (int i=0; i<array.length; ++i) {
 			if(i == 0) {
-				System.out.print(arr[i]);
+				System.out.print(array[i]);
 			}else{
 				System.out.print(" : ");
-				System.out.print(arr[i]);
+				System.out.print(array[i]);
 			}
 		}
 	}
 }
 
-/* geht nur bis zahl 256*/
+/* geht nur mit bis zu 9-stelligen positiven Zahlen*/
